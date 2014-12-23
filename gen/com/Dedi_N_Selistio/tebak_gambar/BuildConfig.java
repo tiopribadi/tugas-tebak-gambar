e32e516bc148f033b607fc97b1c11c8764820f51
tiopribadi <tio_pribadi@ymail.com>
2014-12-23 17:19:14 +0700

    tebakgambarversi1

---
+++ gen/com/Dedi_N_Selistio/tebak_gambar/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.Dedi_N_Selistio.tebak_gambar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/Dedi_N_Selistio/tebak_gambar/NasibActivity.java
@@ -0,0 +1,64 @@
+package com.Dedi_N_Selistio.tebak_gambar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NasibActivity extends Activity {
+	EditText jawabanTxt;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_nasib);
+		//cari komponen edit text1
+        jawabanTxt = (EditText)findViewById(R.id.editText1);
+        //set input huruf besar
+        jawabanTxt.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
+    }
+		
+    public void cekjawaban(View v){
+    	String jawabanBenar = "NASIB BURUK";
+    	String jawabanUser =jawabanTxt.getText().toString();
+    	if(jawabanUser.equals(jawabanBenar)){
+    		//pesan toast
+    		
+    		Toast.makeText(getApplicationContext(),
+    				"JAWABAN TEPAT",Toast.LENGTH_LONG).show();
+    		Intent i = new Intent(getApplicationContext(),SambelActivity.class);
+    		startActivity(i);
+    	}
+    	else{
+    		//pesan toast
+    		Toast.makeText(getApplicationContext(),
+    				"JAWABAN MASIH SALAH",Toast.LENGTH_LONG).show();
+    	}
+    
+    	
+	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.nasib, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
